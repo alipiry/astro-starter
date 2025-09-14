3b006f2d7ddfa27a15c6decd5ba8ae36a745d754
Ali Piry <alipirydev@gmail.com>
2025-09-14 17:29:40 +0330

    add seo stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,19 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import robotsTxt from "astro-robots-txt";
+import sitemap from "@astrojs/sitemap";
+
+import { loadEnv } from "vite";
+
+const { PUBLIC_SITE_URL } = loadEnv(
+  process.env.NODE_ENV || "development",
+  process.cwd(),
+  "",
+);
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: PUBLIC_SITE_URL || "http://localhost:4321",
+  integrations: [sitemap(), robotsTxt()],
+});
